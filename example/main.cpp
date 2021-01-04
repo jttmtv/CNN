8cc0ea4a9163f2a09168cfeeb9fe9d8210c30a32
jttmtv <52265704+jttmtv@users.noreply.github.com>
2021-01-04 17:26:22 +0800

    Add files via upload

--- example/main.cpp
+++ example/main.cpp
@@ -1,1 +1,16 @@
+#include "cnn.hpp"
 
+using namespace std;
+using namespace cv;
+
+
+int main()
+{
+	Img img;
+	float* score;
+	img.scanner("C:\\Users\\layer\\Desktop\\bg.jpg");
+	score = img.facedect();
+	cout << score[0] << endl;
+	cout << score[1] << endl;
+	return 0;
+}
